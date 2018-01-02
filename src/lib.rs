a81cd3d110fb16af145d1f06d7edcad9d2e8a350
freestrings <freestrings@gmail.com>
2018-01-02 22:12:39 +0900

    Random값 배열 합산으로 변경

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,59 @@
+use std::mem;
 use std::ffi::CString;
-use std::os::raw::c_char;
+use std::os::raw::{c_char, c_void};
+use std::vec::Vec;
+use std::slice;
 
-#[no_mangle]
-pub fn sum_in_rust(num: i32) -> i32 {
-    let mut sum = 0;
-    for i in 0..num {
-        sum += i;
-    }
-    sum
-}
-
-fn inline_sum(iter: i32, num: i32) -> u64 {
-    let mut sum: u64 = 0;
-    for _ in 0..iter {
-        sum += sum_in_rust(num) as u64;
-    }
-    sum
-}
-
-#[no_mangle]
-pub fn inline_sum_str_in_rust(iter: i32, num: i32) -> *mut c_char {
-    let s = format!("{}", inline_sum(iter, num));
+fn str_to_raw(s: String) -> *mut c_char {
     let s = CString::new(s).unwrap();
     s.into_raw()
+}
+
+fn sum_array_in_rust(ptr: *mut c_void, len: usize) -> i64 {
+
+    let array: &[u8] = unsafe {
+        mem::transmute(slice::from_raw_parts(ptr, len))
+    };
+
+    let mut sum: i64 = 0;
+    let len = array.len();
+    let mut i = 0;
+    while i < len {
+        let mut v = (array[i + 3] & 0xff) as i32;
+        v = v | ((array[i + 2] & 0xff) as i32) << 8;
+        v = v | ((array[i + 1] & 0xff) as i32) << 16;
+        v = v | ((array[i] & 0xff) as i32) << 24;
+        i += 4;
+        sum += v as i64;
+    }
+    sum
+}
+
+#[no_mangle]
+pub fn inline_sum_array_in_rust_str(iter: i32, ptr: *mut c_void, len: usize) -> *mut c_char {
+    let mut sum: i64 = 0;
+    for _ in 0..iter {
+        sum += sum_array_in_rust(ptr, len);
+    }
+    str_to_raw(format!("{}", sum))
+}
+
+#[no_mangle]
+pub fn sum_array_in_rust_str(ptr: *mut c_void, len: usize) -> *mut c_char {
+    str_to_raw(format!("{}", sum_array_in_rust(ptr, len)))
+}
+
+#[no_mangle]
+pub fn alloc(size: usize) -> *mut c_void {
+    let mut buf = Vec::with_capacity(size);
+    let ptr = buf.as_mut_ptr();
+    mem::forget(buf);
+    return ptr as *mut c_void;
 }
 
 #[no_mangle]
 pub fn dealloc_str(ptr: *mut c_char) {
     unsafe {
         let _ = CString::from_raw(ptr);
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn testa() {
-        assert_eq!(49950000000, super::inline_sum(100000, 1000));
     }
 }
--- www/main.js
+++ www/main.js
@@ -1,41 +1,79 @@
 async function main() {
     return fetchAndInstantiate('testa.wasm').then(module => {
-        const iter = 100000;
-        const sumNumber = 1000;
-        const sumResult = 499500;
+        const iter = 10;
+        const sumNumber = 100000000;
 
-        run('JS - sumInJavascript', iter, () => sumInJavascript(sumNumber) === sumResult);
-        run('JS - inlineSumInJavascript', 1, () => inlineSumInJavascript(iter, sumNumber) === iter * sumResult);
-        run('Rust - sumInRust', iter, () => module.sum_in_rust(sumNumber) === sumResult);
-        run('Rust - inlineSumInRust', 1, () => copyCStr(module, module.inline_sum_str_in_rust(iter, sumNumber)) == iter * sumResult);
+        let sumTargetArray = initArray(sumNumber);
+        let sumResult = sumInJavascript(sumTargetArray);
+        let copiedArrayPtr = copyArray(module, sumTargetArray);
+        let sumResultInRust = sumInRust(module, copiedArrayPtr, sumTargetArray.length * 4);
+
+        if(sumResult != sumResultInRust) {
+            throw new Error(`Different result: ${sumResult}, ${sumResultInRust}`);
+        }
+
+        run('JS - sumInJavascript', iter, () => sumInJavascript(sumTargetArray) === sumResult);
+        run('JS - inlineSumInJavascript', 1, () => inlineSumInJavascript(iter, sumTargetArray) === iter * sumResult);
+        run('Rust - sumInRust', iter, () => sumInRust(module, copiedArrayPtr, sumTargetArray.length * 4) === sumResultInRust);
+        run('Rust - inlineSumInRust', 1, () => inlineSumInRust(module, iter, copiedArrayPtr, sumTargetArray.length * 4) == iter * sumResultInRust);
     });
 }
 
 main().catch(err => console.error(err));
 
+function initArray(num) {
+    let array = new Int32Array(num);
+    for (let i = 0, len = array.length; i < len; i++) {
+        array[i] = ((Math.random() * 20000) | 0) - 10000;
+    }
+    return array;
+}
+
+function copyArray(module, array) {
+    let ptr = module.alloc(array.length * 4);
+    let memory = new Int8Array(module.memory.buffer);
+    let len = array.length;
+    for (let i = 0; i < len; i++) {
+        let v = array[i];
+        for(let j = 3 ; j >= 0 ; j--) {
+            memory[ptr + i * 4 + j] = v & 0xff;
+            v = v >> 8;
+        }
+    }
+    return ptr;
+}
+
 function run(message, iter, cb) {
     let d = Date.now();
-    for (var i = 0; i < iter; i++) {
+    for (let i = 0; i < iter; i++) {
         let ret = cb();
         if (ret !== true) throw new Error(cb.toString());
     }
     console.log(message, Date.now() - d);
 }
 
-function inlineSumInJavascript(iter, num) {
+function inlineSumInJavascript(iter, array) {
     let s = 0;
-    for (var i = 0; i < iter; i++) {
-        s += sumInJavascript(num);
+    for (let i = 0; i < iter; i++) {
+        s += sumInJavascript(array);
     }
     return s;
 }
 
-function sumInJavascript(num) {
+function sumInJavascript(array) {
     let s = 0;
-    for (var i = 0; i < num; i++) {
-        s += i;
+    for (let i = 0, len = array.length; i < len; i++) {
+        s += array[i];
     }
     return s;
+}
+
+function sumInRust(module, arrayPtr, len) {
+    return copyCStr(module, module.sum_array_in_rust_str(arrayPtr, len));
+}
+
+function inlineSumInRust(module, iter, arrayPtr, len) {
+    return copyCStr(module, module.inline_sum_array_in_rust_str(iter, arrayPtr, len));
 }
 
 function fetchAndInstantiate(url, importObject) {
@@ -53,7 +91,7 @@ function copyCStr(module, ptr) {
     const collectCString = function* () {
         let memory = new Uint8Array(module.memory.buffer);
         while (memory[ptr] !== 0) {
-            if (memory[ptr] === undefined) { throw new Error("Woop~ a undefined memory"); }
+            if (memory[ptr] === undefined) { throw new Error(`Undefined: ${ptr}`); }
             yield memory[ptr]
             ptr += 1;
         }
