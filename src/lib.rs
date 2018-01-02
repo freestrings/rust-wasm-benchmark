2368f2f98057ec4b39186a1b9dd891ab5b66a1e0
freestrings <freestrings@gmail.com>
2018-01-02 12:36:57 +0900

    InlineSum 테스트 코드 수정

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::ffi::CString;
+use std::os::raw::c_char;
+
 #[no_mangle]
 pub fn sum_in_rust(num: i32) -> i32 {
     let mut sum = 0;
@@ -7,15 +10,32 @@ pub fn sum_in_rust(num: i32) -> i32 {
     sum
 }
 
-#[no_mangle]
-pub fn inline_sum_in_rust(iter: i32, num: i32) -> i32 {
-    let mut last_sum = 0;
+fn inline_sum(iter: i32, num: i32) -> u64 {
+    let mut sum: u64 = 0;
     for _ in 0..iter {
-        let mut sum = 0;
-        for i in 0..num {
-            sum += i;
-        }
-        last_sum = last_sum.max(sum);
+        sum += sum_in_rust(num) as u64;
     }
-    last_sum
+    sum
+}
+
+#[no_mangle]
+pub fn inline_sum_str_in_rust(iter: i32, num: i32) -> *mut c_char {
+    let s = format!("{}", inline_sum(iter, num));
+    let s = CString::new(s).unwrap();
+    s.into_raw()
+}
+
+#[no_mangle]
+pub fn dealloc_str(ptr: *mut c_char) {
+    unsafe {
+        let _ = CString::from_raw(ptr);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn testa() {
+        assert_eq!(49950000000, super::inline_sum(100000, 1000));
+    }
 }
--- www/main.js
+++ www/main.js
@@ -3,25 +3,36 @@ async function main() {
         const iter = 100000;
         const sumNumber = 1000;
         const sumResult = 499500;
-        console.log('JS - sumInJavascript', runIter(iter, () => sumInJavascript(sumNumber) === sumResult));
-        console.log('Rust - sumInRust', runIter(iter, () => module.sum_in_rust(sumNumber) === sumResult));
-        console.log('Rust - InlineSumInRust', runIter(1, () => module.inline_sum_in_rust(iter, sumNumber) === sumResult));
+
+        run('JS - sumInJavascript', iter, () => sumInJavascript(sumNumber) === sumResult);
+        run('JS - inlineSumInJavascript', 1, () => inlineSumInJavascript(iter, sumNumber) === iter * sumResult);
+        run('Rust - sumInRust', iter, () => module.sum_in_rust(sumNumber) === sumResult);
+        run('Rust - inlineSumInRust', 1, () => copyCStr(module, module.inline_sum_str_in_rust(iter, sumNumber)) == iter * sumResult);
     });
 }
 
 main().catch(err => console.error(err));
 
-function runIter(iter, fn) {
-    var d = Date.now();
-    Array(iter).fill().forEach(() => {
-        if(fn.call() !== true) throw new Error(fn.toString()) 
-    });
-    return Date.now() - d;
+function run(message, iter, cb) {
+    let d = Date.now();
+    for (var i = 0; i < iter; i++) {
+        let ret = cb();
+        if (ret !== true) throw new Error(cb.toString());
+    }
+    console.log(message, Date.now() - d);
+}
+
+function inlineSumInJavascript(iter, num) {
+    let s = 0;
+    for (var i = 0; i < iter; i++) {
+        s += sumInJavascript(num);
+    }
+    return s;
 }
 
 function sumInJavascript(num) {
-    var s = 0;
-    for(var i = 0 ; i < num ; i++) {
+    let s = 0;
+    for (var i = 0; i < num; i++) {
         s += i;
     }
     return s;
@@ -32,7 +43,25 @@ function fetchAndInstantiate(url, importObject) {
         response.arrayBuffer()
     ).then(bytes =>
         WebAssembly.instantiate(bytes, importObject)
-    ).then(results =>
-        results.instance.exports
-    );
+        ).then(results =>
+            results.instance.exports
+        );
+}
+
+function copyCStr(module, ptr) {
+    let orig_ptr = ptr;
+    const collectCString = function* () {
+        let memory = new Uint8Array(module.memory.buffer);
+        while (memory[ptr] !== 0) {
+            if (memory[ptr] === undefined) { throw new Error("Woop~ a undefined memory"); }
+            yield memory[ptr]
+            ptr += 1;
+        }
+    }
+
+    const buffer_as_u8 = new Uint8Array(collectCString())
+    const utf8Decoder = new TextDecoder("UTF-8");
+    const buffer_as_utf8 = utf8Decoder.decode(buffer_as_u8);
+    module.dealloc_str(orig_ptr);
+    return buffer_as_utf8
 }
